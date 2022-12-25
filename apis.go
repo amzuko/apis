e0fd8dc40983726bff5a416949018feff016766c
Andrew Zukoski <andrew@join.build>
2022-12-25 17:17:11 -0600

    Blow away old fuzz tests, add intersection operation

--- apis.go
+++ apis.go
@@ -11,13 +11,15 @@ type bound int
 const (
 	lower = iota
 	upper
+	// TODO: consider having explicit inclusion and exclusion bounds?
+	// This might simplify some of the bookkeeping around whether the 'both' bound is in-set or out-of-set.
 	both
 )
 
 type item struct {
-	// TODO: these are all closed sets. Open sets too.
 	b bound
 	d apd.Decimal
+	open bool
 }
 
 type Set struct {
@@ -31,21 +33,22 @@ func (s *Set) String() string {
 		switch c.b {
 		// Ignoring all errors
 		case lower:
-			if currentInSet {
-				panic("should never happen")
-			} else {
-				if i > 0 {
-					b.WriteString((", "))
-				}
-				_, _ = b.WriteString(fmt.Sprintf("[%v, ", &c.d))
+			if i > 0 {
+				b.WriteString((", "))
 			}
+			if c.open {
+				b.WriteString(("("))
+			} else {
+				b.WriteString(("["))
+			}
+			_, _ = b.WriteString(fmt.Sprintf("%v, ", &c.d))
 			currentInSet = true
 		case upper:
-			if currentInSet {
-				_, _ = b.WriteString(fmt.Sprintf("%v]", &c.d))
+			_, _ = b.WriteString(fmt.Sprintf("%v", &c.d))
+			if c.open {
+				b.WriteString((")"))
 			} else {
-				panic("should never happen")
-
+				b.WriteString(("]"))
 			}
 			currentInSet = false
 		case both:
@@ -55,7 +58,7 @@ func (s *Set) String() string {
 				if i > 0 {
 					b.WriteString((", "))
 				}
-				_, _ = b.WriteString(fmt.Sprintf("(%v, %v)", &c.d, &c.d))
+				_, _ = b.WriteString(fmt.Sprintf("[%v, %v]", &c.d, &c.d))
 			}
 		}
 	}
@@ -80,6 +83,12 @@ func (s *Set)Validate() (error) {
 			}
 		}
 
+		if v.d.Form == apd.Infinite {
+			if !v.open {
+				return fmt.Errorf("%v/%v is infinite, so cannot be closed", i, &v.d)
+			}
+		}
+
 		switch v.b {
 		case lower:
 			if currentInSet {
@@ -98,7 +107,9 @@ func (s *Set)Validate() (error) {
 		case both:
 			// In the case of being inset, a bound that is "both" is an excluded discrete number.
 			// In the case of being out of set, a bound that is "both" is an included discrete number.
-
+			if v.open {
+				return fmt.Errorf("%v/%v is an exclusion or inclusion, so cannot be open", i, &v.d)
+			}
 		}
 
 		// Reset
@@ -112,29 +123,31 @@ func (s *Set)Validate() (error) {
 	return nil
 }
 
-func New(ls string, us string) (Set, error) {
-	l, _, err := apd.BaseContext.NewFromString(ls)
-	if err != nil {
-		return Set{}, err
-	}
-	u, _, err := apd.BaseContext.NewFromString(us)
-	if err != nil {
-		return Set{}, err
-	}
+func New(l apd.Decimal, lOpen bool, u apd.Decimal, uOpen bool) Set {
 	c := apd.Decimal{}
-	apd.BaseContext.Cmp(&c, l, u)
+	apd.BaseContext.Cmp(&c, &l, &u)
+
+
+	// Make inputs well formed
+	if l.Form == apd.Infinite {
+		lOpen = true
+	}
+	if u.Form == apd.Infinite {
+		uOpen = true
+	}
 	
 	var s Set
 	if c.IsZero() {
 		if l.Form == apd.Infinite && u.Form == apd.Infinite {
 			// If we have a set that only contains the infinite value, make it empty
-			return Set{}, nil
+			return Set{}
 		}
 		s = Set{
 			items: []item{
 				{
-				d: *l,
+				d: l,
 				b: both,
+				open: false, // by definition
 				},
 			},
 		}
@@ -143,12 +156,14 @@ func New(ls string, us string) (Set, error) {
 			s = Set{
 				items: []item{
 					{
-					d: *l,
+					d: l,
 					b: lower,
+					open: lOpen,
 					},
 					{
-					d: *u,
+					d: u,
 					b: upper,
+					open: uOpen,
 					},
 				},
 			}
@@ -157,18 +172,32 @@ func New(ls string, us string) (Set, error) {
 			s = Set{
 				items: []item{
 					{
-					d: *u,
+					d: u,
 					b: lower,
+					open: uOpen,
 					},
 					{
-					d: *l,
+					d: l,
 					b: upper,
+					open: lOpen,
 					},
 				},
 			}
 		}
 	}
-	return s, nil
+	return s
+}
+
+func NewFromStrings(ls string, us string) (Set, error) {
+	l, _, err := apd.BaseContext.NewFromString(ls)
+	if err != nil {
+		return Set{}, err
+	}
+	u, _, err := apd.BaseContext.NewFromString(us)
+	if err != nil {
+		return Set{}, err
+	}
+	return New(*l, false, *u, false), nil
 }
 
 func isNegativeInfinity(d apd.Decimal) bool{
@@ -195,26 +224,31 @@ func (a Set) Complement() Set {
 
 
 	if len(a.items) == 1 {
-		// Special case
+		// Special case. The 
 		newItems = append(newItems, item{
 			d: negativeInfinity,
 			b: lower,
+			open: true, // By definiton
 		})
 		newItems = append(newItems, item{
 			d: a.items[0].d,
 			b: both,
+			open: false, // By definition
 		})
 		newItems = append(newItems, item{
 			d: positiveInfinity,
 			b: upper,
+			open: true, // By definiton
 		})
 	} else {
 		for i, v := range a.items {
 			if v.b == both {
+				// No need to change open/closed, as singular values are by definition closed.
 				if i == 0 {
 					newItems = append(newItems, item{
 						d: negativeInfinity,
 						b: lower,
+						open:true, // By definition
 					})
 					newItems = append(newItems, v)
 				} else if i == len(a.items) - 1 {
@@ -222,6 +256,7 @@ func (a Set) Complement() Set {
 					newItems = append(newItems, item{
 						d: positiveInfinity,
 						b: upper,
+						open:true, // By definition
 					})
 				} else {
 					newItems = append(newItems, v)
@@ -236,16 +271,19 @@ func (a Set) Complement() Set {
 						newItems = append(newItems, item{
 							d: negativeInfinity,
 							b: lower,
+							open: true, // by definition
 						})
 						newItems = append(newItems, item{
 							d: v.d,
 							b: upper,
+							open: !v.open,
 						})
 					}
 				} else {
 					newItems = append(newItems, item{
 						d:v.d,
 						b:upper,
+						open: !v.open,
 					})
 				}
 			} else {
@@ -256,10 +294,12 @@ func (a Set) Complement() Set {
 						newItems = append(newItems, item{
 							d: v.d,
 							b: lower,
+							open: !v.open,
 						})
 						newItems = append(newItems, item{
 							d: positiveInfinity,
 							b: upper,
+							open: true,
 						})
 
 					}
@@ -267,6 +307,7 @@ func (a Set) Complement() Set {
 					newItems = append(newItems, item{
 						d:v.d,
 						b:lower,
+						open: !v.open,
 					})
 				}
 			}
@@ -278,8 +319,208 @@ func (a Set) Complement() Set {
 	}
 }
 
+func (a Set)Intersection(b Set) Set {
+	newItems := []item{}
+	ai := 0
+	bi := 0
 
-// a is modified in place
+	aCurrentInSet := false
+	bCurrentInSet := false
+
+	var c apd.Decimal
+
+	for {
+		if ai >= len(a.items)  || bi >= len(b.items){
+			// No more intersections possible.
+			break
+		}
+
+		av := a.items[ai]
+		bv := b.items[bi]
+
+		// TODO: errors
+		_, _ = apd.BaseContext.Cmp(&c, &av.d, &bv.d)
+
+		if c.IsZero() {
+			if av.b == both {
+				if aCurrentInSet {
+					// Exclusion in A
+					if bv.b == both {
+						// Exlcuded in both
+						if bCurrentInSet {
+							newItems = append(newItems, av)
+						} else {
+							// Inclusion in B, exclusion in A, skip as no set is open.
+						}
+					} else if bv.b == lower {
+						// Not currently in any set, boundary is excluded, but A is not in-set above the point
+						newItems = append(newItems, item {
+							lower,
+							bv.d,
+							true,
+						})
+					} else if bv.b == upper {
+						// Interval is terminating, boundary value explictly excluded from A, so boundary is open.
+						newItems = append(newItems, item {
+							upper,
+							bv.d,
+							true,
+						})
+					}
+				} else {
+					// Inclusion in A
+					if bv.b == both {
+						if bCurrentInSet {
+							// Exclusion in B, no set is open, no need to exclude.
+						} else {
+							// Both explicitly included.
+							newItems = append(newItems, av)
+						}
+					} else if bv.b == lower {
+						newItems = append(newItems, item{
+							both,
+							av.d,
+							false,
+						})
+					} else if bv.b == upper {
+						// Intersection not in current interval as A not in current internal
+					}
+				}
+			} else if av.b == lower {
+				if bv.b == lower {
+					// Both opening, interval is closed if both are closed
+					newItems = append(newItems, item{
+						lower,
+						av.d,
+						av.open || bv.open,
+					})
+				} else if bv.b == upper {
+					// A terminating, b starting
+					// If neither is open
+					if !(av.open || bv.open) {
+						newItems = append(newItems, item{
+							both,
+							av.d,
+							false,	// by defintiion
+						})
+					}
+				} else if bv.b == both {
+					if bCurrentInSet {
+						// Exclusion in B, A is beginning, 
+						newItems = append(newItems, item {
+							lower,
+							av.d,
+							true,
+						})
+					} else {
+						// Inclusion in B, open interval in A
+						if !av.open {
+							newItems = append(newItems, item {
+								both,
+								av.d,
+								false,
+							})
+						}
+					}
+				}
+			} else if av.b == upper{
+				// terminating A
+				if bv.b == lower {
+					if !(av.open || bv.open) {
+						newItems = append(newItems, item {
+							both,
+							av.d,
+							false,
+						})
+					}
+				} else if bv.b == upper {
+					newItems = append(newItems, item{
+						upper,
+						av.d,
+						av.open || bv.open,
+					})
+				} else if bv.b == both {
+					if bCurrentInSet {
+						newItems = append(newItems, item{
+							upper,
+							av.d,
+							true,
+						})
+					} else {
+						if !av.open {
+							newItems = append(newItems, item{
+								both,
+								av.d,
+								false,
+							})
+						}
+					}
+				}
+			}
+
+
+			if av.b == lower {
+				aCurrentInSet = true
+			} else if av.b == upper {
+				aCurrentInSet = false
+			}
+
+			if bv.b == lower {
+				bCurrentInSet = true
+			} else if bv.b == upper {
+				bCurrentInSet = false
+			}
+
+			ai++
+			bi++
+			continue
+		}
+
+		if c.Negative {
+			// fmt.Println("In negative")
+			// process a
+			if bCurrentInSet {
+				// If a is termianting, terminate both,
+				// If a is beginning, begin both,
+				// If a is an exlcusion or inclusion, it applies.
+				newItems = append(newItems, av)
+			} else {
+				// Need both to be active
+			}
+			switch av.b {
+			case both:
+				// Nothing
+			case lower:
+				aCurrentInSet = true
+			case upper:
+				aCurrentInSet = false
+			}
+			ai++
+		} else {
+			// fmt.Println("In positive")
+			// process b
+			if aCurrentInSet {
+				newItems = append(newItems, bv)
+			} else {
+			
+			}
+			switch bv.b {
+			case both:
+				// Nothing
+			case lower:
+				bCurrentInSet = true
+			case upper:
+				bCurrentInSet = false
+			}
+			bi++
+		}
+
+	}
+
+	return Set{newItems}
+}
+
+
 func (a Set)Union(b Set) Set {
 
 	newItems := []item{}
@@ -321,8 +562,6 @@ func (a Set)Union(b Set) Set {
 		_, _ = apd.BaseContext.Cmp(&c, &av.d, &bv.d)
 
 		if c.IsZero() {
-			// process both?
-			// 16 different combinations?
 			// fmt.Println("In equal")
 			if av.b == both {
 				if aCurrentInSet {
@@ -336,14 +575,23 @@ func (a Set)Union(b Set) Set {
 							// skip
 						}
 					} else if bv.b == lower {
-						// TODO: open closed sets change this
-						// Skip all, excluded in A but incl;uded in B, set is open going forward
+						if bv.open {
+							// The exlcusion is not included in the opening B interval
+							newItems = append(newItems, av)
+						} else {
+							// B includes the exlcusion in A, keep the interval open./
+						}
 					} else {
-						// TODO: open/closed sets change this
 						// b is leaving set, a will remain open 
-						newItems = append(newItems, av)
+						if bv.open {
+							// b does not contain the value
+							newItems = append(newItems, av)
+						} else {
+							// Skip
+						}
 					}
 				} else {
+					// Inclusion in A
 					if bv.b == both {
 						if bCurrentInSet {
 							// Exlusion in B, inclusion in A. Cancel each other out -- skip
@@ -351,52 +599,92 @@ func (a Set)Union(b Set) Set {
 							// Both explictly included
 							newItems = append(newItems, av)
 						}
-					} else if bv.b == lower {
-						// TODO: open closed sets change this
-						// Open set, don't need to explicitly include
-						newItems = append(newItems, bv)
 					} else {
-						// TODO: open/closed sets change this
-						// b is leaving set, a will remain open 
-						newItems = append(newItems, bv)
+						// REgardless of whether b was open or closed, this boundary is now closed because 
+						// A explicitly inclues that value
+						newItems = append(newItems, item{
+							bv.b,
+							bv.d,
+							false,	// The boundary point is included. 
+						})
 					}
 				}
 			} else if av.b == lower {
 				// fmt.Println("avb lower")
 
 				if bv.b == lower  {
-					// Identical
-					newItems = append(newItems, bv)
+					// Both lower bounds, set is closed if either are closed
+					newItems = append(newItems, item{
+						bv.b,
+						bv.d,
+						bv.open && av.open,
+					})
 				} else if bv.b == upper {
-					// Skip both 
-					// TODO: open/closed changes this
+					// If  both are open, need a new inclusion
+					if bv.open && av.open {
+						newItems = append(newItems, item{
+							both,
+							bv.d,
+							false,
+						})
+					} else {
+						// skip both, 
+					}
 				} else {
 					if bCurrentInSet {
-						// Exclusion in b, inclusion in A (if closed)
-						// Skip both
+						// Exclusion in b, end of A
+						if !av.open {
+							// Skip both
+						} else {
+							newItems = append(newItems, bv)
+						}
 					} else {
 						// Inclusion in B, set opening A
-						newItems = append(newItems, av)
+						// By definition closed
+						newItems = append(newItems, item{
+							av.b,
+							av.d,
+							false,
+						})
 					}
 				}
 			} else {
 				// fmt.Println("avb upper")
-
+				// A ending
 				if bv.b == upper  {
-					// Identical
-					newItems = append(newItems, bv)
+					newItems = append(newItems, item{
+						bv.b,
+						bv.d,
+						bv.open && av.open,
+					})
 				} else if bv.b == lower {
-					// fmt.Println("bvb lower")
-					// Skip both 
-					// TODO: open/closed changes this
+					// B begining
+					if av.open && bv.open {
+						newItems = append(newItems, item{
+							both,
+							bv.d,
+							false,
+						})
+					} else {
+						// AT least one is closed, skip both
+					}
 				} else {
 					if bCurrentInSet {
+						if av.open {
+							newItems = append(newItems, bv)
+						} else {
+							// Skip -- point is included in A, so interval continues
+						}
 						// Exclusion in b, inclusion in A (if closed)
 						// Skip both
 					} else {
 						// Inclusion in B, set closing A
-						// TODO: changes with open/closed
-						newItems = append(newItems, av)
+						// result is always closed
+						newItems = append(newItems, item{
+							av.b,
+							av.d,
+							false,
+						})
 					}
 				}
 			}
@@ -441,7 +729,6 @@ func (a Set)Union(b Set) Set {
 			} else {
 				newItems = append(newItems, bv)
 			}
-			// process a
 			switch bv.b {
 			case both:
 				// Nothing
--- apis_test.go
+++ apis_test.go
@@ -1,62 +1,82 @@
 package apis
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/cockroachdb/apd/v3"
+)
+
+type boundDef struct {
+	s string
+	o bool
+}
+
+func newFromBounds(b1 boundDef, b2 boundDef) Set {
+	l, _, _ := apd.BaseContext.NewFromString(b1.s)
+	u, _, _ := apd.BaseContext.NewFromString(b2.s)
+	return New(*l, b1.o, *u, b2.o)
+}
 
 func TestCreationAndComplement(t *testing.T) {
-	cases := [][]string {
-		{"-infinity", "0", "[-Infinity, 0]", "[0, Infinity]"},
-		{"3", "3.0", "(3, 3)", "[-Infinity, 3), (3, Infinity]"},
-		{"1", "2.0", "[1, 2.0]", "[-Infinity, 1], [2.0, Infinity]"},
+	type testcase struct {
+		b1 boundDef
+		b2 boundDef
+		creationResult string
+		complementResult string
+	}
+	cases := []testcase {
+		{boundDef{"-infinity", true}, boundDef{"0", false}, "(-Infinity, 0]", "(0, Infinity)"},
+		{boundDef{"-infinity", true}, boundDef{"0", true}, "(-Infinity, 0)", "[0, Infinity)"},
+		{boundDef{"3", false}, boundDef{"3.0", false}, "[3, 3]", "(-Infinity, 3), (3, Infinity)"},
+		{boundDef{"1", false}, boundDef{"2.0", false}, "[1, 2.0]", "(-Infinity, 1), (2.0, Infinity)"},
 	}
 
 	for _, c := range cases {
-		t.Run(c[0]+"+"+c[1], func(t *testing.T) {
-			s, err := New(c[0], c[1])
-			if err != nil {
-				t.Fatal(err)
-			}
-			err = s.Validate()
+		t.Run(c.creationResult, func(t *testing.T) {
+			s := newFromBounds(c.b1, c.b2)
+
+			err := s.Validate()
 			if err != nil {
 				t.Fatal(err)
 			}
 			r := s.String()
-			if r != c[2] {
-				t.Fatalf("Expected creation '%v', but got '%v'", c[2], r)
+			if r != c.creationResult {
+				t.Fatalf("Expected creation '%v', but got '%v'", c.creationResult, r)
 			}
 
 			comp := s.Complement()
 			r = comp.String()
-			if r!=c[3] {
-				t.Fatalf("Expected complement'%v', but got '%v'", c[3], r)
+			if r!=c.complementResult {
+				t.Fatalf("Expected complement'%v', but got '%v'", c.complementResult, r)
 			}
 		})
 	}
 }
 
 func TestUnion(t *testing.T) {
-	cases := [][]string{
-		{"-infinity", "0", "0", "infinity", "[-Infinity, Infinity]"},
-		{"0", "1", "2", "3", "[0, 1], [2, 3]"},
-		{"0", "1", "1", "1", "[0, 1]"},
-		{"1", "1", "0", "1", "[0, 1]"},
-		{"-infinity", "2", "0", "infinity", "[-Infinity, Infinity]"},
+
+	type testcase struct {
+		b []boundDef
+		unionResult string
+	}
+
+	cases := []testcase{
+		{[]boundDef{{"-infinity", true}, { "0", false}, {"0", false}, {"infinity", true}}, "(-Infinity, Infinity)"},
+		{[]boundDef{{"0", false}, { "1", false}, {"2", false}, {"3", true}}, "[0, 1], [2, 3)"},
+		{[]boundDef{{"0", false}, { "1", true}, {"1", false}, {"1", false}}, "[0, 1]"},
+		{[]boundDef{{"-infinity", true}, { "2", false}, {"0", false}, {"infinity", true}}, "(-Infinity, Infinity)"},
 	}
 
 	for _, c := range cases {
-		t.Run(c[4], func(t *testing.T) {
-			l, err := New(c[0], c[1])
-			if err != nil {
-				t.Fatal(err)
-			}
-			err = l.Validate()
+		t.Run(c.unionResult, func(t *testing.T) {
+			l := newFromBounds(c.b[0], c.b[1])
+			err := l.Validate()
 			if err != nil {
 				t.Fatal(err)
 			}
 
-			m, err := New(c[2], c[3])
-			if err != nil {
-				t.Fatal(err)
-			}
+			m := newFromBounds(c.b[2], c.b[3])
+
 			err = m.Validate()
 			if err != nil {
 				t.Fatal(err)
@@ -65,47 +85,87 @@ func TestUnion(t *testing.T) {
 			n := l.Union(m)
 
 			r := n.String()
-			if r != c[4] {
-				t.Fatalf("Expected '%v', but got '%v'", c[2], r)
+			if r != c.unionResult {
+				t.Fatalf("Expected '%v', but got '%v'", c.unionResult, r)
 			}
 		})
 	}
 }
 
 
-func getValue(b byte) string {
-	switch b%8 {
-	case 0:
-		return "-Infinity"
-	case 1:
-		return "0"
-	case 2:
-		return "2"
-	case 3:
-		return "3"
-	case 4:
-		return "4"
-	case 5:
-		return "5"
-	case 6:
-		return "6"
-	case 7:
-		return "Infinity"
+func TestIntersect(t *testing.T) {
+
+	type testcase struct {
+		b []boundDef
+		intersectionResult string
 	}
-	panic("should never happen")
+
+	cases := []testcase{
+		{[]boundDef{{"0", false}, { "0", false}, {"0", false}, {"infinity", true}}, "[0, 0]"},
+	}
+
+	for _, c := range cases {
+		t.Run(c.intersectionResult, func(t *testing.T) {
+			l := newFromBounds(c.b[0], c.b[1])
+			err := l.Validate()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			m := newFromBounds(c.b[2], c.b[3])
+
+			err = m.Validate()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			n := l.Intersection(m)
+
+			r := n.String()
+			if r != c.intersectionResult {
+				t.Fatalf("Expected '%v', but got '%v'", c.intersectionResult, r)
+			}
+		})
+	}
 }
 
+
+
+func getValue(b byte) apd.Decimal {
+	var d *apd.Decimal
+	switch b%8 {
+	case 0:
+		d, _, _ = apd.BaseContext.NewFromString("-Infinity")
+	case 1:
+		d, _, _ = apd.BaseContext.NewFromString( "0")
+	case 2:
+		d, _, _ = apd.BaseContext.NewFromString( "2")
+	case 3:
+		d, _, _ = apd.BaseContext.NewFromString( "3")
+	case 4:
+		d, _, _ = apd.BaseContext.NewFromString( "4")
+	case 5:
+		d, _, _ = apd.BaseContext.NewFromString( "5")
+	case 6:
+		d, _, _ = apd.BaseContext.NewFromString( "6")
+	case 7:
+		d, _, _ = apd.BaseContext.NewFromString( "Infinity")
+	}
+	return *d
+}
+
+func getOpen(b byte) bool {
+	return b%2 == 1
+}
 
 func FuzzOperations(f *testing.F) {
 	f.Fuzz(func(t *testing.T, b []byte) {
 		// Create an initial set
-		if len(b) < 2 {
+		if len(b) < 4 {
 			t.SkipNow()
 		}
-		s, err := New(getValue(b[0]), getValue(b[1]))
-		if err != nil {
-			t.Fatal(err)
-		}
+		s := New(getValue(b[0]), getOpen(b[1]), getValue(b[2]), getOpen(b[2]))
+
 		t.Logf("Created '%v'", s.String())
 
 		i := 3
@@ -113,45 +173,60 @@ func FuzzOperations(f *testing.F) {
 			if i >= len(b) {
 				break
 			}
-			switch(b[i]%3){
+			switch(b[i]%5){
 			case 0:
 				s = s.Complement()
-				t.Logf("Completment '%v'", s.String())
+				t.Logf("Completment")
+				t.Logf("Result '%v'", s.String())
 
 				i++
 			case 1:
-				if i + 2 >= len(b) {
-					i += 3
+				if i + 4 >= len(b) {
+					i += 5
 					break
 				}
-				u, err := New(getValue(b[i+1]), getValue(b[i+2]))
+				u := New(getValue(b[i+1]), getOpen(b[i+2]), getValue(b[i+3]), getOpen(b[i+4]))
 				t.Logf("Union with '%v'", u.String())
 
-				if err != nil {
-					t.Fatal(err)
-				}
 				s = s.Union(u)
 				t.Logf("Result '%v'", s.String())
 
-				i+=3
+				i+=5
 			case 2:
-				if i + 2 >= len(b) {
-					i += 3
+				if i + 4 >= len(b) {
+					i += 5
 					break
 				}
-				u, err := New(getValue(b[i+1]), getValue(b[i+2]))
+				u := New(getValue(b[i+1]), getOpen(b[i+2]), getValue(b[i+3]), getOpen(b[i+4]))
 				t.Logf("Union with complement of '%v'", u.String())
 
-				if err != nil {
-					t.Fatal(err)
-				}
 				s = s.Union(u.Complement())
 				t.Logf("Result '%v'", s.String())
-				i += 3
+				i += 5
+			case 3:
+				if i + 4 >= len(b) {
+					i += 5
+					break
+				}
+				u := New(getValue(b[i+1]), getOpen(b[i+2]), getValue(b[i+3]), getOpen(b[i+4]))
+				t.Logf("Intersect with  '%v'", u.String())
+				s = s.Intersection(u)
+				t.Logf("Result '%v'", s.String())
+				i += 5
+			case 4:
+				if i + 4 >= len(b) {
+					i += 5
+					break
+				}
+				u := New(getValue(b[i+1]), getOpen(b[i+2]), getValue(b[i+3]), getOpen(b[i+4]))
+				t.Logf("Intersect with  complement of '%v'", u.String())
+				s = s.Intersection(u.Complement())
+				t.Logf("Result '%v'", s.String())
+				i += 5
 			}
 		}
 
-		err = s.Validate()
+		err := s.Validate()
 		if err != nil {
 			t.Fatal(err)
 		}
